a73cae366e309afe017608c847567531d1e29b99
Artem Struts <strutsart7@gmail.com>
2021-06-09 17:51:05 +0300

    add option in gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,10 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
 
+program
+  .option('-f, --format [type]', 'output format')
+
+program
+  .arguments('<filepath1> <filepath2>')
+
 program.parse()
