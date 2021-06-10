d113023c7e82a87465c003bc06e788aca1bf8d97
Artem Struts <strutsart7@gmail.com>
2021-06-10 14:53:26 +0300

    add comparison flat data

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import { readFileSync } from 'fs';
+// import path from 'path';
+import genDiff from '../src/genDiff.js';
 
 program
   .version('1.0.0')
@@ -11,5 +14,15 @@ program
 
 program
   .arguments('<filepath1> <filepath2>')
+
+program
+  .action((filepath1, filepath2) => {
+    const jsonData1 = readFileSync(filepath1);
+    const jsonData2 = readFileSync(filepath2);
+    const jsonParse1 = JSON.parse(jsonData1);
+    const jsonParse2 = JSON.parse(jsonData2);
+    const result = genDiff(jsonParse1, jsonParse2);
+    console.log(result);
+  }) 
 
 program.parse()
